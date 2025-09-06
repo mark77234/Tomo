6f56ee67281d9a522be2b7b7d6ab7ed95d0b3fe3
tomchccom <dreamkms2014@gmail.com>
2025-09-06 20:46:39 +0900

    feat: 초기설정

---
+++ backend/src/main/java/com/example/tomo/TomoApplication.java
@@ -0,0 +1,13 @@
+package com.example.tomo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TomoApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/example/tomo/TomoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tomo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
