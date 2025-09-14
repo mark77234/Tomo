ab6b3c1d5f535255122233b039075852220eb95b
tomchccom <dreamkms2014@gmail.com>
2025-09-14 17:56:33 +0900

    chore: Oauth 로그인 파일 제거 - 삭제되지 않고 남아있는 Oauth코드가 커밋에 반영됨 - 삭제 후 커밋 적용

--- demo/src/main/java/com/example/demo/repository/RefreshTokenRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.RefreshToken;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken, String> {
-}
