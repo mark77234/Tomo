a464bebe678178107a9dc3ea4d428237c640501a
mark <mark77234@naver.com>
2025-09-18 22:59:36 +0900

    feat: Custom 색상 컴포넌트 추가 및 구글아이콘 추가

---
+++ frontend/app/src/main/java/com/markoala/tomoandroid/ui/theme/CustomColor.kt
@@ -0,0 +1,10 @@
+package com.markoala.tomoandroid.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+object CustomColor {
+    val lightGray = Color(0xFFD1D5DB)
+    val mediumGray = Color(0xFF6B7280)
+    // 필요시 다른 색상도 추가 가능
+}
+
