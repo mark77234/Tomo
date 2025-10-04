90cd8ed771e8516dab2a4c4e70ea4428611926a3
mark <mark77234@naver.com>
2025-10-04 21:37:27 +0900

    feat: 로그인 CustomToast 추가

--- frontend/app/src/main/java/com/markoala/tomoandroid/MainActivity.kt
+++ frontend/app/src/main/java/com/markoala/tomoandroid/MainActivity.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.navigation.compose.rememberNavController
 import com.markoala.tomoandroid.auth.AuthManager
 import com.markoala.tomoandroid.navigation.AppNavHost
+import com.markoala.tomoandroid.ui.components.ToastProvider
 import com.markoala.tomoandroid.ui.theme.TomoAndroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,8 +37,10 @@ fun MainScreen() {
 //    val scope = rememberCoroutineScope()
     val navController = rememberNavController()
 
-    // 로그인/로그아웃 시 signedIn 상태 변경
-    AppNavHost(navController = navController, isSignedIn = signedIn)
+    ToastProvider {
+        // 로그인/로그아웃 시 signedIn 상태 변경
+        AppNavHost(navController = navController, isSignedIn = signedIn)
+    }
 
     // 로그인 성공/실패 콜백을 NavHost에서 처리하므로 아래 기존 UI는 제거
 }
