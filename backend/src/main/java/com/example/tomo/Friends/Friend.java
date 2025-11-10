ba89b2bed75ce0b67c547110a4b21dc19f25f8c6
tomchccom <dreamkms2014@gmail.com>
2025-11-10 23:15:46 +0900

    fix : setter 제거

--- backend/src/main/java/com/example/tomo/Friends/Friend.java
+++ backend/src/main/java/com/example/tomo/Friends/Friend.java
@@ -4,15 +4,12 @@ package com.example.tomo.Friends;
 import com.example.tomo.Users.User;
 import jakarta.persistence.*;
 import lombok.Getter;
-import lombok.Setter;
-
 import java.time.LocalDate;
 
 
 
 @Entity
 @Getter
-@Setter
 @Table(name = "friend")
 public class Friend {
 
