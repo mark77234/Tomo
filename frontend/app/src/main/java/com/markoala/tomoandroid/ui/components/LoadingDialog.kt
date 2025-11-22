444a9c07d4a12c5ef32fc9872af14bc98e8ce43a
mark <mark77234@naver.com>
2025-11-22 15:06:52 +0900

    fix: 불필요한 임포트문 제거

--- frontend/app/src/main/java/com/markoala/tomoandroid/ui/components/LoadingDialog.kt
+++ frontend/app/src/main/java/com/markoala/tomoandroid/ui/components/LoadingDialog.kt
@@ -10,19 +10,14 @@ import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 
 @Composable
