51afe12481b5ddf9cb344a496f27fc892aec3513
mark <mark77234@naver.com>
2025-11-23 22:30:32 +0900

    refactor: 안쓴는 임포트문 제거

--- frontend/app/src/main/java/com/markoala/tomoandroid/ui/main/calendar/CalendarScreen.kt
+++ frontend/app/src/main/java/com/markoala/tomoandroid/ui/main/calendar/CalendarScreen.kt
@@ -1,35 +1,22 @@
 package com.markoala.tomoandroid.ui.main.calendar
 
-import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.KeyboardArrowLeft
-import androidx.compose.material.icons.rounded.KeyboardArrowRight
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.markoala.tomoandroid.ui.components.CustomText
 import com.markoala.tomoandroid.ui.components.CustomTextType
 import com.markoala.tomoandroid.ui.main.calendar.components.TomoCalendar
 import com.markoala.tomoandroid.ui.theme.CustomColor
-import com.markoala.tomoandroid.util.generateCalendarMatrix
-import java.time.DayOfWeek
 import java.time.LocalDate
 import java.time.YearMonth
 
