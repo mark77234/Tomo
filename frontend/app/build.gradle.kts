6fa31934f94a742209baeec0d51ce2288bc760ef
mark <mark77234@naver.com>
2025-11-30 22:46:17 +0900

    chore: 버전 업데이트 (1.1.5)

--- frontend/app/build.gradle.kts
+++ frontend/app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.markoala.tomoandroid"
         minSdk = 33
         targetSdk = 36
-        versionCode = 14
-        versionName = "1.1.4"
+        versionCode = 15
+        versionName = "1.1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
