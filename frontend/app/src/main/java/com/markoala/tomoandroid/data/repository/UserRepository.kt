0b6ba127198f75404b0ea861d9f52649d78ee422
mark <mark77234@naver.com>
2025-12-01 23:48:33 +0900

    feat: 로그인 시 firestore에 fcm 토큰 필드 업데이트

--- frontend/app/src/main/java/com/markoala/tomoandroid/data/repository/UserRepository.kt
+++ frontend/app/src/main/java/com/markoala/tomoandroid/data/repository/UserRepository.kt
@@ -1,6 +1,7 @@
 package com.markoala.tomoandroid.data.repository
 
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.SetOptions
 import com.markoala.tomoandroid.data.model.user.UserProfile
 import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlin.coroutines.resume
@@ -10,7 +11,7 @@ object UserRepository {
     /**
      * Firestore에 사용자 정보 저장 (suspend)
      */
-    suspend fun saveUserToFirestore(userProfile: UserProfile) =
+    suspend fun saveUserToFirestore(userProfile: UserProfile, fcmToken: String? = null) =
         suspendCancellableCoroutine<Unit> { cont ->
             try {
                 val firestore = FirebaseFirestore.getInstance()
@@ -19,12 +20,37 @@ object UserRepository {
                     "name" to userProfile.username,
                     "email" to userProfile.email
                 )
-                val task = firestore.collection("users").document(userProfile.uuid).set(userMap)
+                if (!fcmToken.isNullOrBlank()) {
+                    userMap["fcmToken"] = fcmToken
+                }
+                val task = firestore.collection("users")
+                    .document(userProfile.uuid)
+                    .set(userMap, SetOptions.merge())
                 task.addOnSuccessListener {
                     cont.resume(Unit)
                 }.addOnFailureListener { ex ->
                     cont.resumeWithException(ex)
                 }
+            } catch (e: Exception) {
+                cont.resumeWithException(e)
+            }
+        }
+
+    suspend fun updateFcmToken(uuid: String, fcmToken: String) =
+        suspendCancellableCoroutine<Unit> { cont ->
+            try {
+                val firestore = FirebaseFirestore.getInstance()
+                val task = firestore.collection("users")
+                    .document(uuid)
+                    .set(
+                        mapOf(
+                            "uid" to uuid,
+                            "fcmToken" to fcmToken
+                        ),
+                        SetOptions.merge()
+                    )
+                task.addOnSuccessListener { cont.resume(Unit) }
+                    .addOnFailureListener { ex -> cont.resumeWithException(ex) }
             } catch (e: Exception) {
                 cont.resumeWithException(e)
             }
